15afebad16b5691154a8b11f4ab800f75a081360
Andrew Tavis McAllister <andrew.t.mcallister@gmail.com>
2024-01-26 11:44:38 +0100

    Add in Wikimedia favicon and Tailwind base styles

--- frontend/src/views/ChatView.vue
+++ frontend/src/views/ChatView.vue
@@ -1,6 +1,6 @@
 <template>
-  <main>
-    <p class="text-2xl">ChatGSWiki</p>
+  <main class="h-screen bg-light-content dark:bg-dark-content">
+    <p class="text-light-text dark:text-dark-text text-2xl">ChatGSWiki</p>
   </main>
 </template>
 
