6a2995c98bb35b4d85f2f615f56345261d3d739f
roti <robert.timm@wikimedia.de>
2024-02-01 22:52:31 +0000

    chore: remove debug/test code

--- gswikichat/vector_store_interface.py
+++ gswikichat/vector_store_interface.py
@@ -1,16 +1,12 @@
 import os
 import json
 
-# from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
 
 from haystack import Document  # , Pipeline
 from haystack.components.embedders import SentenceTransformersDocumentEmbedder
-# from haystack.components.embedders import SentenceTransformersTextEmbedder
 from haystack.document_stores.in_memory import InMemoryDocumentStore
-# from haystack.components.retrievers.in_memory import InMemoryBM25Retriever
 from haystack.components.retrievers.in_memory import InMemoryEmbeddingRetriever
-# from haystack.components.writers import DocumentWriter
 from haystack.document_stores.types.policy import DuplicatePolicy
 
 HUGGING_FACE_HUB_TOKEN = os.environ.get('HUGGING_FACE_HUB_TOKEN')
@@ -64,54 +60,9 @@ document_store = InMemoryDocumentStore(
     # embedding_dim=768,
     # duplicate_documents="overwrite"
 )
-# document_store.write_documents(input_documents)
-
-# TODO Introduce Jina.AI from HuggingFace. Establish env-variable for trust_...
-
-# basic_transformer_models = [
-#     "all-MiniLM-L6-v2",
-#     "xlm-clm-ende-1024",
-#     "xlm-mlm-ende-1024",
-#     "bert-base-german-cased",
-#     "bert-base-german-dbmdz-cased",
-#     "bert-base-german-dbmdz-uncased",
-#     "distilbert-base-german-cased",
-#     "xlm-roberta-large-finetuned-conll03-german",
-#     "deutsche-telekom/gbert-large-paraphrase-cosine"
-# ]
-
-# https://huggingface.co/sentence-transformers/all-MiniLM-L6-v2
-# sentence_transformer_model = "all-MiniLM-L6-v2"
-# 3 minutes to batch 82
-
-# https://huggingface.co/deutsche-telekom/gbert-large-paraphrase-cosine
-# sentence_transformer_model = 'deutsche-telekom/gbert-large-paraphrase-cosine'
-# 76 minutes to batch 82
-
-# https://huggingface.co/jinaai/jina-embeddings-v2-base-de
-# sentence_transformer_model = 'jinaai/jina-embeddings-v2-base-de'
-# Cannot find or load the embedding model
-# Unknown minutes to batch 82
-
-# https://huggingface.co/aari1995/German_Semantic_STS_V2
-# sentence_transformer_model = 'aari1995/German_Semantic_STS_V2'
-# 75 minutes to batch 82
-
-# https://huggingface.co/Sahajtomar/German-semantic
-# sentence_transformer_model = 'Sahajtomar/German-semantic'
-# 72 minutes to batch 82
 
 # https://huggingface.co/svalabs/german-gpl-adapted-covid
 sentence_transformer_model = 'svalabs/german-gpl-adapted-covid'
-# 2 minutes to batch 82
-
-# https://huggingface.co/PM-AI/bi-encoder_msmarco_bert-base_german
-# sentence_transformer_model = 'PM-AI/bi-encoder_msmarco_bert-base_german'
-# 14 minutes to batch 82
-
-# https://huggingface.co/JoBeer/german-semantic-base
-# sentence_transformer_model = 'JoBeer/german-semantic-base'
-# 22 minutes to batch 82
 
 print(f'Sentence Transformer Name:{sentence_transformer_model}')
 
@@ -122,26 +73,9 @@ embedder = SentenceTransformersDocumentEmbedder(
     # token=HUGGING_FACE_HUB_TOKEN
 )
 
-# hg_embedder = SentenceTransformer(
-#     "jinaai/jina-embeddings-v2-base-de",
-#     token=HUGGING_FACE_HUB_TOKEN
-# )
-
 embedder.warm_up()
 
 documents_with_embeddings = embedder.run(input_documents)
-# documents_with_embeddings = embedder.encode(input_documents)
-
-
-# print('\n\n')
-# # print(documents_with_embeddings['documents'])
-# print(type(documents_with_embeddings['documents']))
-# print(len(documents_with_embeddings['documents']))
-# print(dir(documents_with_embeddings['documents'][0]))
-# print('\n\n')
-# print(type(embedder.model))
-# print('\n\n')
-# # print(dir(hg_embedder))
 
 
 document_store.write_documents(
@@ -155,14 +89,3 @@ retriever = InMemoryEmbeddingRetriever(
     top_k=top_k
 )
 
-# writer = DocumentWriter(document_store=document_store)
-
-# indexing_pipeline = Pipeline()
-# indexing_pipeline.add_component("embedder", embedder)
-# indexing_pipeline.add_component("writer", writer)
-# indexing_pipeline.connect("embedder", "writer")
-# indexing_pipeline.run(
-#     {
-#         "embedder": {"documents": input_documents}
-#     }
-# )
