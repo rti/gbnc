74a0cb528f7a235909a9f42b5c9909a54c20785f
Robert Timm <mail@rtti.de>
2024-02-05 08:24:35 +0000

    chore: remove comment

--- gswikichat/llm_config.py
+++ gswikichat/llm_config.py
@@ -1,7 +1,6 @@
 import os
 from haystack_integrations.components.generators.ollama import OllamaGenerator
 
-# TODO: discolm prompt https://huggingface.co/DiscoResearch/DiscoLM_German_7b_v1
 print(f"Setting up ollama with {os.getenv('MODEL')}")
 llm = OllamaGenerator(
     model=os.getenv("MODEL"),
