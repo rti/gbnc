22b04d0ad0d2f77281063e317e2e6b1bbd6eea66
Jonathan Fraine <jdfraine@gmail.com>
2024-02-09 17:41:44 +0100

    added logging via logger with Handler to api.py; PEP8 formatted api.py

--- gswikichat/llm_config.py
+++ gswikichat/llm_config.py
@@ -1,6 +1,9 @@
 import os
 from haystack_integrations.components.generators.ollama import OllamaGenerator
 
+# import logging
+# logger = logging.getLogger()
+
 print(f"Setting up ollama with {os.getenv('MODEL')}")
 llm = OllamaGenerator(
     model=os.getenv("MODEL"),
