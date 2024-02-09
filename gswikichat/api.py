9ab649f98f519614323d3172e2ae954eeaff9c5b
Jonathan Fraine <jdfraine@gmail.com>
2024-02-09 20:17:08 +0100

    created a rag_pipeline in the rag.py based on the usage in api.py; removed rag_piipeline from api.py; introduced rag_pipeline from rag.py into api.py

--- gswikichat/api.py
+++ gswikichat/api.py
@@ -2,10 +2,7 @@ from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
 
-from .rag import answer_builder
-from .llm_config import llm
-from .prompt import prompt_builders
-from .vector_store_interface import embedder, retriever, input_documents
+from .rag import rag_pipeline
 
 from haystack import Document
 from .logger import get_logger
@@ -40,50 +37,13 @@ async def api(query, top_k=3, lang='en'):
 
     logger.debug(f'{query=}')  # Assuming we change the input name
     logger.debug(f'{top_k=}')
-    logger.debug(f'{top_k=}')
+    logger.debug(f'{lang=}')
 
-    query = Document(content=query)
-
-    query_embedded = embedder.run([query])
-    query_embedding = query_embedded['documents'][0].embedding
-
-    retriever_results = retriever.run(
-        query_embedding=list(query_embedding),
-        filters=None,
+    answer = rag_pipeline(
+        query=query,
         top_k=top_k,
-        scale_score=None,
-        return_embedding=None
+        lang=lang
     )
-
-    logger.debug('retriever results:')
-    for retriever_result_ in retriever_results:
-        logger.debug(retriever_result_)
-
-    prompt_builder = prompt_builders[lang]
-
-    prompt_build = prompt_builder.run(
-        question=query.content,  # As a Document instance, .content returns a string
-        documents=retriever_results['documents']
-    )
-
-    prompt = prompt_build['prompt']
-
-    logger.debug(f'{prompt=}')
-
-    response = llm.run(prompt=prompt, generation_kwargs=None)
-
-    answer_build = answer_builder.run(
-        query=query.content,  # As a Document class, .content returns the string
-        replies=response['replies'],
-        meta=response['meta'],
-        documents=retriever_results['documents'],
-        pattern=None,
-        reference_pattern=None
-    )
-
-    logger.debug(f'{answer_build=}')
-
-    answer = answer_build['answers'][0]
 
     sources = [
         {
