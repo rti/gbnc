a04effc137894fe18093f8cb15fc57d3b7f45fb2
roti <robert.timm@wikimedia.de>
2024-02-12 22:29:39 +0000

    feat(start.sh): pull llm using ollama (if not built into container)
    closes #35

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -e
+
 if [[ $PUBLIC_KEY ]]
 then
     mkdir -p ~/.ssh
     chmod 700 ~/.ssh
     cd ~/.ssh
-    echo $PUBLIC_KEY >> authorized_keys
+    echo "$PUBLIC_KEY" >> authorized_keys
     chmod 700 -R ~/.ssh
     cd /
     service ssh start
@@ -16,10 +18,17 @@ fi
 echo "Starting ollama"
 ollama serve &
 
+while ! curl "$OLLAMA_URL"; do 
+    sleep 1 
+done
+
+echo "Pulling $OLLAMA_MODEL_NAME from ollama library"
+ollama pull "$OLLAMA_MODEL_NAME"
+
 cd /workspace
 
 echo "Starting api"
 uvicorn gswikichat:app --reload --host 0.0.0.0 --port 8000 &
 
-echo "Sleeping..."
+echo "Ready"
 sleep infinity
